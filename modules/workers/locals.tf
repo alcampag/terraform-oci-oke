913b7937c10daa88e08c2035cebf805f3416b5d8
alcampag <alberto.campagna@oracle.com>
2025-11-03 10:20:02 +0100

    fixed lookup function and implemented try instead to safely access data.oci_core_image.workers

--- modules/workers/locals.tf
+++ modules/workers/locals.tf
@@ -305,9 +305,18 @@ locals {
     for k, v in local.enabled_worker_pools : k => {
       kubernetes_major_version = substr(lookup(v, "kubernetes_version", ""), 1, 4)
       kubernetes_minor_version = substr(lookup(v, "kubernetes_version", ""), 1, -1)
-      ubuntu_release           = lookup(lookup(data.oci_core_image.workers, k, {}), "operating_system_version", null) != null ? lookup(lookup(data.oci_core_image.workers, k, {}), "operating_system_version") : lookup(v, "os_version", null)
+      ubuntu_release = try(
+        lookup(try(data.oci_core_image.workers[k], {}), "operating_system_version", null),
+        lookup(v, "os_version", null)
+      )
     }
     if lookup(v, "mode", var.worker_pool_mode) != "virtual-node-pool" &&
-    contains(coalescelist(split(" ", lookup(lookup(data.oci_core_image.workers, k, {}), "operating_system", "")), [lookup(v, "os", "")]), "Ubuntu")
+    contains(
+      coalescelist(
+        split(" ", try(lookup(try(data.oci_core_image.workers[k], {}), "operating_system", ""), lookup(v, "os", ""))),
+        [lookup(v, "os", "")]
+      ),
+      "Ubuntu"
+    )
   }
 }
